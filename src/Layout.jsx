38ce0a9c14281d2b3a678ed81a4d8021fe643df1
0x5b3a <101mailme@protonmail.com>
2024-07-11 13:17:17 +0530

    added vercel analytics

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,14 @@
 import {Outlet} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { Analytics } from '@vercel/analytics/react'
 function Layout() {
   return (
     <>
     <Header/>
     <Outlet/>
     <Footer/>
+    <Analytics mode='production'/>
     </>
   )
 }
