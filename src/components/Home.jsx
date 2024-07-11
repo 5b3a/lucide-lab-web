f8988b6b1089b56f48ff510b19328a599b3052c3
0x5b3a <101mailme@protonmail.com>
2024-07-11 20:23:32 +0530

    added search box

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,8 @@ import {
 } from "@radix-ui/themes";
 import * as Label from "@radix-ui/react-label";
 
-import { Icon } from "lucide-react";
+import { Icon, Search } from "lucide-react";
+
 import { nanoid } from "nanoid";
 
 import { Link } from "react-router-dom";
@@ -18,18 +19,42 @@ import { Link } from "react-router-dom";
 import { useState, useMemo } from "react";
 
 function Home() {
-  const iconArray = Object.keys(lucideLabs);
+  const allIcons = Object.keys(lucideLabs);
+  const [iconArray, setIconArray] = useState(Object.keys(lucideLabs));
   const [strokeWidth, setStrokeWidth] = useState(2);
   const [size, setSize] = useState(36);
   const [color, setColor] = useState("currentColor");
   const [abswidth, setAbswidth] = useState(false);
-
+  const handleSearch = useMemo(
+    () => (e) => {
+      const newArr = iconArray.filter((item) => item.includes(e.target.value));
+      e.target.value.length > 0 ? setIconArray(newArr) : setIconArray(allIcons);
+    },
+    [iconArray, allIcons]
+  );
   return (
     <Section className=" p-3 m-3 rounded-sm flex flex-col flex-wrap justify-center items-center gap-4">
-      {/* insert searchbox */}
-
+      {/* upper section searchbox and icons */}
+      <Flex dir="row" gap={"3"}>
+        {/* search box */}
+        <Box className="flex flex-row gap-2 bg-[var(--red-a2)] rounded-md">
+          {/* input searchox */}
+          <input
+            placeholder="search icons..."
+            className="p-2 bg-transparent italic font-mono"
+            onChange={handleSearch}
+          />
+          {/* search icon */}
+          <Search className="m-auto mx-auto mr-2" size={25} />
+        </Box>
+        {/* icons count */}
+        <Box className="font-mono bg-[var(--white-a2)] p-2 rounded-md gap-2">
+          <Label.Root>Showing icons : {iconArray.length}</Label.Root>
+        </Box>
+      </Flex>
       {/* seprator */}
       <Separator orientation={"horizontal"} className="w-[90%]" />
+
       <Flex justify={"center"} align={"center"} gap={"3"} wrap={"wrap"}>
         {/* stroke width */}
         <Box className=" bg-[var(--red-a2)] p-2 flex flex-wrap rounded-md justify-center items-center gap-2">
@@ -105,17 +130,13 @@ function Home() {
       <Separator orientation={"horizontal"} className="w-[90%]" />
 
       {/* main grid */}
-      <Grid
-        columns={{ initial: "4", sm: "4", lg: "12" }}
-        gap="4"
-        className="mt-6"
-      >
+      <Grid columns={{ initial: "5", lg: "12" }} gap="4" className="mt-6">
         {iconArray.map((item) => {
           return (
             <Link
               to={`icons/${item}`}
               key={nanoid()}
-              className="flex items-center justify-center p-4 border border-[var(--red-)] bg-[var(--red-a2)] rounded-md flex-col gap-2 text-[var(--red-11)]"
+              className="flex items-center justify-center p-4 border border-[var(--red-)] bg-[var(--red-a2)] rounded-md flex-col gap-2 text-[var(--red-11)] hover:bg-[var(--red-a5)] def-tr-clr"
               title={item.toLowerCase()}
             >
               {
